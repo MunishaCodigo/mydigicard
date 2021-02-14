f5aba5ef0e35df5623a4e1b94f5a00ee753dc368
MunishaCodigo <munisha4947@gmail.com>
2021-02-14 01:00:31 +0100

    Updates

---
+++ precache-manifest.3496cb41ba5f8b28a82965012eceab3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d38fbdb3e03389c2e9bf6ac148910a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c00ecdfa73ec979fdc",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c6f140e6caf1d32bd3c2",
+    "url": "/static/css/main.dd9e0c71.chunk.css"
+  },
+  {
+    "revision": "50c00ecdfa73ec979fdc",
+    "url": "/static/js/2.fd8a0951.chunk.js"
+  },
+  {
+    "revision": "ad52c1c3bf5e99e99a21eca4099279dd",
+    "url": "/static/js/2.fd8a0951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f140e6caf1d32bd3c2",
+    "url": "/static/js/main.f8b34e76.chunk.js"
+  },
+  {
+    "revision": "21f5f3feb7033687a55c",
+    "url": "/static/js/runtime-main.b29c06ce.js"
+  }
+]);
